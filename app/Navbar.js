5cd66c5d27605014e1641aea8aa957039c08972c
Frankenstein256 <frankletsu60@gmail.com>
2026-09-13 02:19:58 +0000

    Update Navbar.js

--- app/Navbar.js
+++ app/Navbar.js
@@ -27,12 +27,15 @@ export default function Navbar() {
       <Link href="/messages" style={{ textDecoration: 'none', color: '#1A1A1A' }}>
         Messages
       </Link>
-          <Link href="/save" style={{ textDecoration: 'none', color: '#1A1A1A' }}>
-  Save
-</Link>
-    <Link href="/rent" style={{ textDecoration: 'none', color: '#1A1A1A' }}>
-  Rent
-</Link>
+      <Link href="/save" style={{ textDecoration: 'none', color: '#1A1A1A' }}>
+        Save
+      </Link>
+      <Link href="/rent" style={{ textDecoration: 'none', color: '#1A1A1A' }}>
+        Rent
+      </Link>
+      <Link href="/learn" style={{ textDecoration: 'none', color: '#1A1A1A' }}>
+        Learn
+      </Link>
     </nav>
   );
   }
